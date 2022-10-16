5ffd598fb4bf56dc6faf6cffdd09be4b7ad9dea4
Kevin Amado <kamadorueda@gmail.com>
2022-10-15 20:10:27 -0600

    feat: just one tutorial

--- flake.nix
+++ flake.nix
@@ -9,26 +9,20 @@
     nixpkgs = import inputs.nixpkgs {inherit system;};
   in {
     devShells.${system}.default = nixpkgs.mkShell {
-      name = "tsu-resources";
+      name = "docs";
       packages = [
         nixpkgs.mdbook
       ];
     };
 
     packages.${system}.default = nixpkgs.stdenv.mkDerivation {
-      name = "tsu-resources";
+      name = "docs";
       src = ./.;
 
       buildInputs = [nixpkgs.mdbook];
 
-      builder = builtins.toFile "builder.sh" ''
-        source $stdenv/setup
-
-        for project in open-sourcing-like-a-pro; do
-          cd "$src/$project"
-          mdbook build --dest-dir "$out/$project"
-        done
-      '';
+      buildPhase = "mdbook build --dest-dir $out";
+      dontInstall = true;
     };
   };
 }
