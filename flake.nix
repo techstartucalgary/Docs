82b6c1f50b8ab0d6ad58f8b2af7cd7209daf865a
Kevin Amado <kamadorueda@gmail.com>
2022-10-26 23:19:55 -0600

    feat: simplify dev env

--- flake.nix
+++ flake.nix
@@ -7,23 +7,22 @@
     system = "x86_64-linux";
 
     nixpkgs = import inputs.nixpkgs {inherit system;};
+
+    dependencies = [
+      nixpkgs.mdbook
+      nixpkgs.mdbook-linkcheck
+    ];
   in {
     devShells.${system}.default = nixpkgs.mkShell {
       name = "docs";
-      packages = [
-        nixpkgs.mdbook
-        nixpkgs.mdbook-linkcheck
-      ];
+      packages = dependencies;
     };
 
     packages.${system}.default = nixpkgs.stdenv.mkDerivation {
       name = "docs";
       src = ./.;
 
-      buildInputs = [
-        nixpkgs.mdbook
-        nixpkgs.mdbook-linkcheck
-      ];
+      buildInputs = dependencies;
 
       buildPhase = "mdbook build --dest-dir $out";
       dontInstall = true;
